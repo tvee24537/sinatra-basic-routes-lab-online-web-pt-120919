c707a90feca3022697c353fd4eecae32da122578
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-17 23:05:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Thanapat"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Pure Imagination"
+  end
+  
+  get '/hometown' do
+    "My hometown is Huntington"
+  end
+  
 end
